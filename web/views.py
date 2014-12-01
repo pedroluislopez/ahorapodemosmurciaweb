f3690f18e2c03635cc389004e826656e2d3f8663
Pedro Lopez <plopez@swissms.ch>
2014-12-01 11:17:08 +0100

    Nuevos textos y correcciones

--- web/views.py
+++ web/views.py
@@ -4,15 +4,27 @@ from web.models import Candidato, IdeaFuerza, Cita, Documento
 # Create your views here.
 def index(request):
     ideasfuerza_m = IdeaFuerza.objects.filter(seccion='m').order_by('orden')
+    m_columns = 0
+    if len(ideasfuerza_m) > 0:
+        m_columns = 12 / len(ideasfuerza_m)
     ideasfuerza_p = IdeaFuerza.objects.filter(seccion='p').order_by('orden')
+    p_columns = 0
+    if len(ideasfuerza_m) > 0:
+        p_columns = 12 / len(ideasfuerza_m)
     ideasfuerza_a = IdeaFuerza.objects.filter(seccion='a').order_by('orden')
+    a_columns = 0
+    if len(ideasfuerza_m) > 0:
+        a_columns = 12 / len(ideasfuerza_m)
     citas = Cita.objects.order_by('orden')
     candidatos = Candidato.objects.order_by('orden')
     documentos = Documento.objects.order_by('orden')
     return render(request, 'index.html', {
         'ideasfuerza_m': ideasfuerza_m,
+        'm_columns': m_columns,
         'ideasfuerza_p': ideasfuerza_p,
+        'p_columns': p_columns,
         'ideasfuerza_a': ideasfuerza_a,
+        'a_columns': a_columns,
         'citas': citas,
         'candidatos': candidatos,
         'documentos': documentos,
