1796eed4dc079a79ab590afd184d29858c562e63
Pedro Lopez <plopez@swissms.ch>
2014-12-13 12:43:41 +0100

    Arreglada navegación blog

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from blog.models import Publicacion
 # Create your views here.
 def index(request, num_page=0):
     ENTRIES_PER_PAGE = 4
+    num_page = int(num_page)
     entry_count = Publicacion.objects.count()
     max_pages = entry_count / 4
     if num_page < 0:
@@ -11,15 +12,17 @@ def index(request, num_page=0):
     if num_page > max_pages:
         num_page = max_pages
     offset = num_page * ENTRIES_PER_PAGE
+    limit = offset + ENTRIES_PER_PAGE
+    print str(offset) + ':' + str(limit)
     
     num_prev = None
     if num_page > 0:
         num_prev = num_page - 1
     num_next = None
-    if entry_count > (offset + ENTRIES_PER_PAGE):
+    if entry_count > limit:
         num_next = num_page + 1
     
-    publicaciones = Publicacion.objects.order_by('-publicado')[offset:ENTRIES_PER_PAGE]
+    publicaciones = Publicacion.objects.order_by('-publicado')[offset:limit]
     
     return render(request, 'blog/index.html', {
         'page': 'blog',
